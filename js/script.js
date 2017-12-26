8d08faf8815858d9aca36b0d46327b3e887bfa5d
Sheryll <1601536d@student.tp.edu.sg>
2017-12-26 12:17:53 +0800

    Changes to Files

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
 $(document).ready(function() {
     // all custom jQuery will go here
-
-    /*Scroll to top when arrow up clicked BEGIN*/
-	$(window).scroll(function() {
-    	var height = $(window).scrollTop();
-    	if (height > 100) {
-        	$('#Go2Top').fadeIn();
-    	} else {
-        	$('#Go2Top').fadeOut();
-    	}
-	});
-	$(document).ready(function() {
-    	$("#Go2Top").click(function(event) {
-        	event.preventDefault();
-        	$("html, body").animate({ scrollTop: 0 }, "slow");
-        	return false;
-    	});
-	});
- /*Scroll to top when arrow up clicked END*/
-
 });
